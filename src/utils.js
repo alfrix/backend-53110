46b4dcc6619bad8908869d5b3ba4cb892b4cb342
alfrix <alfredomonclus@gmail.com>
2024-02-29 13:11:45 -0300

    multer
    This reverts commit 372f180d1550d73df4cfc112ff44367a1acdbcec.

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,17 @@ const rutaproducts = path.join(path.resolve(__dirname, '..'), "data/products.jso
 const rutacarts = path.join(path.resolve(__dirname, '..'), "data/carts.json");
 
 export { __dirname, rutaproducts, rutacarts };
+
+
+import multer from "multer";
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+export { upload }
