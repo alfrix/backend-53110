d17069e573f5ed4e83ea8d6c8cfde34c0a84eb7c
alfrix <alfredomonclus@gmail.com>
2024-04-29 10:06:56 -0300

    fix dotenv

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+const mode = "online"
+
+dotenv.config({ 
+    path: './src/.env',
+    override: true
+});
+
+export const config = {
+    MODO: mode,
+    PORT: process.env.PORT || 8080,
+    mongoUrl: mode==="local"?process.env.MONGO_URL_LOCAL : process.env.MONGO_URL_ATLAS
+}
