291065247ae8430e12a9d63487870070ad47792e
Alfredo Monclus <alfredomonclus@gmail.com>
2024-05-15 15:54:01 -0300

    add ticketmodel

---
+++ src/dao/models/ticketmodel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const ticketsColl = "tickets"
+const ticketsSchema = new mongoose.Schema(
+    {
+        code: { type: String, required: true, unique: true},
+        purchase_datetime: { type: String, required:true},
+        amount: { type: Number, required:true},
+        purchaser: { type: String, required:true },
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const ticketsModel = mongoose.model(ticketsColl, ticketsSchema)
