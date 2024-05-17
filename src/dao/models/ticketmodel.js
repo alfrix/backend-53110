a1811cc8bbd2e191fb8d0c021f93161d96f321bc
Alfredo Monclus <alfredomonclus@gmail.com>
2024-05-17 09:51:54 -0300

    ticketmodel reformat

--- src/dao/models/ticketmodel.js
+++ src/dao/models/ticketmodel.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
-const ticketsColl = "tickets"
+const ticketsColl = "tickets";
 const ticketsSchema = new mongoose.Schema(
-    {
-        code: { type: String, required: true, unique: true},
-        purchase_datetime: { type: String, required:true},
-        amount: { type: Number, required:true},
-        purchaser: { type: String, required:true },
-    },
-    {
-        timestamps: true
-    }
-)
+  {
+    code: { type: String, required: true, unique: true },
+    purchase_datetime: { type: String, required: true },
+    amount: { type: Number, required: true },
+    purchaser: { type: String, required: true }, // correo
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const ticketsModel = mongoose.model(ticketsColl, ticketsSchema)
+export const ticketsModel = mongoose.model(ticketsColl, ticketsSchema);
